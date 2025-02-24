f0beeb11ca6d4993ef26aa70ab7fb73a7ff81e79
Caleb Oki <caleboki@gmail.com>
2025-02-23 22:31:38 -0500

    Database Setup: users, recipes, and selected_recipes

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function selectedRecipes()
+    {
+        return $this->hasMany(SelectedRecipe::class);
+    }
 }
